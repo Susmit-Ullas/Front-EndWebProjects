8af64904b3d9362fcfe1a3db695d319d7bdb6a3c
Susmit Ullas <susmitullas@gmail.com>
2023-01-10 06:58:05 +0530

    Update

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -5,4 +5,3 @@ const container = document.querySelector('.container');
 open.addEventListener('click', () => container.classList.add('show-nav'));
 
 close.addEventListener('click', () => container.classList.remove('show-nav'));
-
